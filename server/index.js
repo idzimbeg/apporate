af6a1bf5be6c72c6137fc1b39517c6d30f529c4c
idzimbeg <igordzimbeg@gmail.com>
2021-11-14 20:40:07 +0100

    server added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const authRoutes = require('./routes/auth.js');
+const authRoutes = require("./routes/auth.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,10 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
-    res.send('Hello, World')
+    res.send('Hello, World!');
 });
+
 app.use('/auth', authRoutes);
 
-app.listen(PORT, () => 
-    console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
